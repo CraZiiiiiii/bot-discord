38d7a8ec70e0239b4871a085cd612474e5462413
CraZiiiiiii <142044613+CraZiiiiiii@users.noreply.github.com>
2025-09-01 18:48:54 +0200

    Update CodePython.py

--- CodePython.py
+++ CodePython.py
@@ -23,7 +23,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print("Bot started successfully !")
+    print("Bot started successfully!")
 
 @bot.command()
 async def prix(ctx, *, nom_annee: str):
@@ -97,5 +97,6 @@ async def main():
     await bot.start(DISCORD_TOKEN)
 
 asyncio.run(main())
+
 
 
